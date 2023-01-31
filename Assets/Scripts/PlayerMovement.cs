11cc0fa4216c099d81f92438e96ec9ea8f4e0c91
Garmacon <luke.greenwood18@gmail.com>
2023-02-01 00:10:31 +0900

    Player Movement
    - Added Player Movement Script

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Rigidbody _rb;
+
+    void Awake()
+    {
+        //grab rigidbody and put into _rb
+    }
+
+    
+}
