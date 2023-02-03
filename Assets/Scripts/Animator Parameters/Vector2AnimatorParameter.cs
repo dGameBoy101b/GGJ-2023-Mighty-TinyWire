687bb69200f93abea23f33e1ced1196e901c55e0
dGameBoy101b <44672112+dGameBoy101b@users.noreply.github.com>
2023-02-03 17:39:07 +0800

    Created vector2 animator parameter.

---
+++ Assets/Scripts/Animator Parameters/Vector2AnimatorParameter.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class Vecto2AnimatorParameter : AnimatorParameter<Vector2>
+{
+	[SerializeField]
+	[Tooltip("The suffix used for the x component")]
+	public string XSuffix = '.x';
+
+	public string XName {get => this.ParameterName + this.XSuffix; }
+
+	[SerializeField]
+	[Tooltip("The suffix used for the y component")]
+	public string YSuffix = '.y';
+
+	public string YName {get => this.ParameterName + this.YSuffix; }
+
+	public Vector2 Value
+	{
+		get => new Vector2(this.Animator.GetFloat(this.XName), this.Animator.GetFloat(this.YName));
+		set
+		{
+			this.Animator.SetFloat(this.XName, value.x);
+			this.Animator.SetFloat(this.YName, value.y);
+		}
+	}
+}
