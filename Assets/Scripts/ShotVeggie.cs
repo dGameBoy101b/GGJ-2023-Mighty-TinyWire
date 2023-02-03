59fb94e9d55f7405b77f0e1a0192b960c8164c64
dGameBoy101b <44672112+dGameBoy101b@users.noreply.github.com>
2023-02-03 23:36:14 +0800

    Added shot carrot settling.

--- Assets/Scripts/ShotVeggie.cs
+++ Assets/Scripts/ShotVeggie.cs
@@ -3,9 +3,10 @@ using UnityEngine.Events;
 
 public class ShotVeggie : MonoBehaviour
 {
+	[Header("Destruction")]
 	[SerializeField]
 	[Tooltip("The mask used to determine what destroys this")]
-	public LayerMask DestructionMask;
+	public LayerMask BreakMask;
 
 	[SerializeField]
 	[Tooltip("Invoked just before this is destroyed")]
@@ -13,14 +14,51 @@ public class ShotVeggie : MonoBehaviour
 
 	public UnityEvent OnBreak { get => this._onBreak; }
 
-	private void OnDestroy()
+	[Header("Settling")]
+	[SerializeField]
+	[Tooltip("The mask used to determine what settles this into its harmless form")]
+	public LayerMask SettleMask;
+
+	[SerializeField]
+	[Tooltip("The prefab spawned when this settles")]
+	public GameObject SettlePrefab;
+
+	[SerializeField]
+	[Tooltip("Invoked when this settles into its harmless form")]
+	private UnityEvent _onSettle = new UnityEvent();
+
+	public UnityEvent OnSettle { get => this._onSettle; }
+
+	[SerializeField]
+	[Tooltip("The rigidbody on this")]
+	private Rigidbody Body;
+
+	public void Settle()
 	{
+		Object.Destroy(this);
+		var settled = Object.Instantiate(this.SettlePrefab, this.transform.position, this.transform.rotation);
+		var body = settled.GetComponent<Rigidbody>();
+		body.velocity = this.Body.velocity;
+		body.angularVelocity = this.Body.angularVelocity;
+		this.OnSettle.Invoke();
+	}
+
+	public void Break()
+	{
+		Object.Destroy(this);
 		this.OnBreak.Invoke();
+	}
+
+	private void Awake()
+	{
+		this.Body ??= this.GetComponent<Rigidbody>();
 	}
 
 	private void OnCollisionEnter(Collision collision)
 	{
-		if (LayerMaskComparer.IsObjectInMask(collision.gameObject, this.DestructionMask))
-			Object.Destroy(this);
+		if (LayerMaskComparer.IsObjectInMask(collision.gameObject, this.BreakMask))
+			this.Break();
+		else if (LayerMaskComparer.IsObjectInMask(collision.gameObject, this.SettleMask))
+			this.Settle();
 	}
 }
