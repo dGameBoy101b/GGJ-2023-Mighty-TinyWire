b13f254fb11bff1c2d5d6994fa3733c1614f0955
dGameBoy101b <44672112+dGameBoy101b@users.noreply.github.com>
2023-02-05 12:41:21 +0800

    Added rat stunning

--- Assets/Scripts/Gun/RatSuckable.cs
+++ Assets/Scripts/Gun/RatSuckable.cs
@@ -1,11 +1,26 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class RatSuckable : PulledSuckable
 {
+	[SerializeField]
+	[Tooltip("The rat script to be stunned")]
+	public Rat Rat;
+
+	public override void StartSuck(Vacuum origin)
+	{
+		this.Rat.Stun();
+		base.StartSuck(origin);
+	}
+
+	public override void StopSuck(Vacuum origin)
+	{
+		this.Rat.ShouldRecover = true;
+		base.StopSuck(origin);
+	}
+
 	public override void HitEnd(Vacuum origin)
 	{
-		throw new System.NotImplementedException();
+		//throw new System.NotImplementedException();
+		base.HitEnd(origin);
 	}
 }
